0a6ae204fbe765eb666ab48770831eae4af2053a
Dmitry <dsheiko@intrigma.com>
2016-06-22 22:13:15 +0200

    Create loading screen layout

--- app/src/main/java/com/moyersoftware/contender/LoadingActivity.java
+++ app/src/main/java/com/moyersoftware/contender/LoadingActivity.java
@@ -1,7 +1,9 @@
 package com.moyersoftware.contender;
 
-import android.support.v7.app.AppCompatActivity;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class LoadingActivity extends AppCompatActivity {
 
@@ -9,5 +11,18 @@ public class LoadingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading);
+
+        initStatusBar();
+    }
+
+    /**
+     * Makes the status bar translucent.
+     */
+    private void initStatusBar() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        }
     }
 }
