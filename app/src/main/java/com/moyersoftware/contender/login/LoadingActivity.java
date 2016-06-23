0d07cd3d165780c473dc234313e6da926deea87a
Dmitry <dsheiko@intrigma.com>
2016-06-23 13:35:34 +0200

    Create register screen layout

---
+++ app/src/main/java/com/moyersoftware/contender/login/LoadingActivity.java
@@ -0,0 +1,38 @@
+package com.moyersoftware.contender.login;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.moyersoftware.contender.R;
+
+public class LoadingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading);
+
+        initStatusBar();
+    }
+
+    /**
+     * Makes the status bar translucent.
+     */
+    private void initStatusBar() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        }
+    }
+
+    /**
+     * Opens the registration screen.
+     */
+    public void onRegisterButtonClicked(View view) {
+        startActivity(new Intent(this, RegisterActivity.class));
+    }
+}
--- app/src/main/java/com/moyersoftware/contender/login/RegisterActivity.java
+++ app/src/main/java/com/moyersoftware/contender/login/RegisterActivity.java
@@ -1,18 +1,28 @@
-package com.moyersoftware.contender;
+package com.moyersoftware.contender.login;
 
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-public class LoadingActivity extends AppCompatActivity {
+import com.moyersoftware.contender.R;
+
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_loading);
+        overrideActivityAnimation();
+        setContentView(R.layout.activity_register);
 
         initStatusBar();
+    }
+
+    /**
+     * Creates a cross fade effect between loading and registration screens.
+     */
+    private void overrideActivityAnimation() {
+        overridePendingTransition(R.anim.activity_fade_in, R.anim.activity_fade_hold);
     }
 
     /**
