9e7d9dbcc94be9ac7c56ed980c8dfe45a693face
Dmitry <dsheiko@intrigma.com>
2016-06-25 12:42:08 +0200

    Implement email/password registration with Firebase

--- app/src/main/java/com/moyersoftware/contender/login/RegisterActivity.java
+++ app/src/main/java/com/moyersoftware/contender/login/RegisterActivity.java
@@ -4,23 +4,85 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.moyersoftware.contender.R;
 import com.moyersoftware.contender.menu.MainActivity;
+import com.moyersoftware.contender.util.Util;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    // Views
+    @Bind(R.id.register_email_edit_txt)
+    EditText mEmailEditTxt;
+    @Bind(R.id.register_password_edit_txt)
+    EditText mPasswordEditTxt;
+    @Bind(R.id.register_repeat_password_edit_txt)
+    EditText mRepeatPasswordEditTxt;
+
+    // Usual variables
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        ButterKnife.bind(this);
 
         overrideActivityAnimation();
+        initAuth();
         initStatusBar();
+    }
+
+    private void initAuth() {
+        // Initialize FirebaseAuth
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Util.Log("onAuthStateChanged:signed_in:" + user.getUid());
+
+                    finish();
+                    // TODO: Finish loading activity
+                    startActivity(new Intent(RegisterActivity.this, MainActivity.class));
+                } else {
+                    // User is signed out
+                    Util.Log("onAuthStateChanged:signed_out");
+                }
+            }
+        };
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
     }
 
     /**
@@ -56,6 +118,38 @@ public class RegisterActivity extends AppCompatActivity {
     }
 
     public void onRegisterButtonClicked(View view) {
-        startActivity(new Intent(this, MainActivity.class));
+        String email = mEmailEditTxt.getText().toString();
+        String password = mPasswordEditTxt.getText().toString();
+        String repeatedPassword = mRepeatPasswordEditTxt.getText().toString();
+        if (TextUtils.isEmpty(email) || TextUtils.isEmpty(password)) {
+            Toast.makeText(RegisterActivity.this, "Some fields are empty", Toast.LENGTH_SHORT)
+                    .show();
+        } else if (!password.equals(repeatedPassword)) {
+            Toast.makeText(RegisterActivity.this, "Passwords don't match", Toast.LENGTH_SHORT)
+                    .show();
+        } else {
+            mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this,
+                    new OnCompleteListener<AuthResult>() {
+                        @SuppressWarnings("ThrowableResultOfMethodCallIgnored")
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            Util.Log("createUserWithEmail:onComplete:" + task.isSuccessful());
+
+                            // If sign in fails, display a message to the user. If sign in succeeds
+                            // the auth state listener will be notified and logic to handle the
+                            // signed in user can be handled in the listener.
+                            if (!task.isSuccessful()) {
+                                if (task.getException() != null && task.getException()
+                                        .getMessage() != null) {
+                                    Toast.makeText(RegisterActivity.this, task.getException()
+                                            .getMessage(), Toast.LENGTH_LONG).show();
+                                } else {
+                                    Toast.makeText(RegisterActivity.this, "Authentication failed",
+                                            Toast.LENGTH_LONG).show();
+                                }
+                            }
+                        }
+                    });
+        }
     }
 }
--- app/src/main/java/com/moyersoftware/contender/menu/MainActivity.java
+++ app/src/main/java/com/moyersoftware/contender/menu/MainActivity.java
@@ -1,13 +1,19 @@
 package com.moyersoftware.contender.menu;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.moyersoftware.contender.R;
+import com.moyersoftware.contender.login.LoadingActivity;
 import com.moyersoftware.contender.menu.adapter.MainPagerAdapter;
+import com.moyersoftware.contender.util.Util;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -27,12 +33,26 @@ public class MainActivity extends AppCompatActivity {
             R.drawable.tab_friends,
             R.drawable.tab_settings
     };
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseUser mFirebaseUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+        // Initialize Firebase Auth
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mFirebaseUser = mFirebaseAuth.getCurrentUser();
+        if (mFirebaseUser == null) {
+            // Not signed in, launch the Sign In activity
+            startActivity(new Intent(this, LoadingActivity.class));
+            finish();
+            return;
+        } else {
+            Util.Log("username = "+mFirebaseUser.getEmail()+", "+mFirebaseUser.getEmail().replace("/@.*/", ""));
+        }
 
         initPager();
         initTabs();
@@ -56,5 +76,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void attachBaseContext(Context newBase) {
         super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
+    }
+
+    public void onLogOutButtonClicked(View view) {
+        mFirebaseAuth.signOut();
+        startActivity(new Intent(this, LoadingActivity.class));
+        finish();
     }
 }
